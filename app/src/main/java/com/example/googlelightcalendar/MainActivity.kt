1f9361ed254b601a4ef207c5a965d8e91c61b80c
samue <samuebrandenburg12@gmail.com>
2024-01-29 13:58:35 -0500

    made Root private

--- app/src/main/java/com/example/googlelightcalendar/MainActivity.kt
+++ app/src/main/java/com/example/googlelightcalendar/MainActivity.kt
@@ -3,52 +3,31 @@ package com.example.googlelightcalendar
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TabPosition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.googlelightcalendar.core.main_screen.MainScreenViewModel
-import com.example.googlelightcalendar.core.toolBarStates.ToolBarState
 import com.example.googlelightcalendar.navigation.components.AuthNavManager
-import com.example.googlelightcalendar.navigation.components.MainScreenNavManager
 import com.example.googlelightcalendar.navigation.components.MainScreenNavigation
 import com.example.googlelightcalendar.navigation.components.NavigationDestinations
-import com.example.googlelightcalendar.navigation.components.ProfileRoutes
 import com.example.googlelightcalendar.navigation.components.screens
 import com.example.googlelightcalendar.navigation.routes.MainScreenRoutes
-import com.example.googlelightcalendar.navigation.routes.ProfileRoutes
 import com.example.googlelightcalendar.navigation.routes.RegisterUserRoutes
-import com.example.googlelightcalendar.screens.food_diary.DiaryScreen
 import com.example.googlelightcalendar.screens.loginScreen.InitialScreen
-import com.example.googlelightcalendar.screens.profile.ProfileScreen
 import com.example.googlelightcalendar.ui.theme.GoogleLightCalendarTheme
 import com.example.googlelightcalendar.ui.theme.appColor
-import com.example.googlelightcalendar.ui.theme.yellowMain
 import com.example.googlelightcalendar.ui_components.bottomBar.ChooseUBottomBar
-import com.example.googlelightcalendar.ui_components.toolbar.ChooseUToolBar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
@@ -78,7 +57,7 @@ class MainActivity : ComponentActivity() {
 
 @RequiresApi(Build.VERSION_CODES.P)
 @Composable
-fun root(
+private fun root(
     navAuthManager: AuthNavManager,
 ) {
     val navControl = rememberNavController()
@@ -129,7 +108,6 @@ fun root(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MainScreen(
     userId: String,
